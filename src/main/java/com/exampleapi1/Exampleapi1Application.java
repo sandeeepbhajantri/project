604970437f72eb1a74e4953f5834f8b1ddd1d9e9
sandeepbhajantri15 <sandeepbhajantri15@gmail.com>
2025-03-29 14:41:24 +0530

    Update Exampleapi1Application.java

--- src/main/java/com/exampleapi1/Exampleapi1Application.java
+++ src/main/java/com/exampleapi1/Exampleapi1Application.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Exampleapi1Application {
+	int x = "110" ;
 	public static void main(String[] args) {
 		SpringApplication.run(Exampleapi1Application.class, args);
 	}
